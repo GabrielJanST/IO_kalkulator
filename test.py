1bb13a36b19770abad6cabbd3b83f2d4be9277d5
Gabriel <gabriel@air-gabriel.mynet>
2022-03-28 20:04:58 +0200

    dodano funkcje witania

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
+
 Python 3.9.1 (v3.9.1:1e5d33e9b9, Dec  7 2020, 12:10:52) 
-[Clang 6.0 (clang-600.0.57)] on darwin
+[Clang 6.0 (clang-600.0.57)] on darwi
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> #To bedzie plik na ktorym bedziemy pracowali z repo
+def hello(name):
+	return "Hello" + str(name)
 >>> print("Hello world")
