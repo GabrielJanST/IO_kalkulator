c4d910d9f242ef7543675b40213d4d89c8789ae9
Gabriel <gabriel@air-gabriel.mynet>
2022-03-28 20:11:18 +0200

    dodano kalkulator z dodawaniem

--- test.py
+++ test.py
@@ -5,4 +5,9 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> #To bedzie plik na ktorym bedziemy pracowali z repo
 def hello(name):
 	return "Hello" + str(name)
->>> print("Hello world")
+def dodaj(a,b):
+	wynik = float(a) + float(b)
+	return wynik
+pierwsza = input()
+druga = input()
+ print(dodaj(pierwsza, druga))
