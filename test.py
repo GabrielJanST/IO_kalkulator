7bdee72d724a53c5a205529fe3ea19121345a36e
Gabriel <gabriel@air-gabriel.mynet>
2022-03-28 19:44:35 +0200

    Pierwszy commit

---
+++ test.py
@@ -0,0 +1,5 @@
+Python 3.9.1 (v3.9.1:1e5d33e9b9, Dec  7 2020, 12:10:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #To bedzie plik na ktorym bedziemy pracowali z repo
+>>> print("Hello world")
