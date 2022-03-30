2c7710ae0f7eae4fbdb936242a32eded9a0a7260
Gabriel <69848@student.ue.poznan.pl>
2022-03-30 19:38:31 +0200

    odejmowanie zaimplementowane

--- test.py
+++ test.py
@@ -5,4 +5,6 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> #To bedzie plik na ktorym bedziemy pracowali z repo
 def hello(name):
 	return "Hello" + str(name)
+def odejmij(a,b):
+	return a-b
 print("Hello world")
